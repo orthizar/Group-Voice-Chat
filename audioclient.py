285507614b7a2271f92a1c0a1c247c9a73615bb9
SilvanKohler <edu.silvan.kohler@gmail.com>
2019-04-11 19:11:16 +0200

    Update audioclient.py

--- audioclient.py
+++ audioclient.py
@@ -8,7 +8,16 @@ CHANNELS = 2
 RATE = 192000
 CHUNK = 4096
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((input("IP:"), int(input("Port:"))))
+if len(sys.argv) > 1:
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+else:
+    IP = input("IP:")
+    PORT = int(input("Port:"))
+s.connect((IP, PORT))
+def callback(in_data, frame_count, time_info, status):
+    s.send(in_data)
+    return (None, pyaudio.paContinue)
 audio = pyaudio.PyAudio()
 streamsend = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK, stream_callback=callback)
 streamrecieve = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
@@ -20,8 +29,5 @@ class recieve(threading.Thread):
         streamrecieve.write(data)
         recieving = recieve()
         recieving.start()
-def callback(in_data, frame_count, time_info, status):
-    s.send(in_data)
-    return (None, pyaudio.paContinue)
 recieving = recieve()
 recieving.start()
